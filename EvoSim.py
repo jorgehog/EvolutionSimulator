66d06cfb76bc5e808be68b913af8330e2521118e
unknown <Jrgen@TheMatrix.(none)>
2012-04-20 23:39:44 +0200

    Working version

--- EvoSim.py
+++ EvoSim.py
@@ -51,7 +51,9 @@ class sinebased(function):
         j = random.randint(2)
 
         if k == max_num_kids:
-            self.parameters[i][j] = random.random();
+            self.parameters[i][j] = random.random()*2 - 1;
+            self.get_values()
+            self.get_rank()
             return None
 
         old = self.rank
@@ -76,7 +78,7 @@ def evolved(population, ranks):
     n = len(population)
     tmp = [0]*n
     ranks.sort()
-
+    
     #Sort by how good the fit were
     for i in range(n):
         rank = ranks[i]
@@ -87,19 +89,19 @@ def evolved(population, ranks):
 
     #Bottom half doesn't make it. They are replaced by a mean over the survivors
     mean_genetic_code = zeros(shape=tmp[0].parameters.shape)
-    for person in tmp[:n/2]:
+    for person in tmp[:10]:
         mean_genetic_code += person.parameters;
-    mean_genetic_code *= 2./n;
+    mean_genetic_code *= 1./10;
         
     for i in range(n/2, n):
         person = eval(population[0].__class__.__name__ + "(mean_genetic_code, population[0].f)");
         person.set_region(-2,2, reset=True)
-        for j in xrange(mutation_thresh):
+        for j in range(mutation_thresh):
             person.evolve_parameter(1);
 
         
         tmp[i] = person;
-    
+
     avg = 0;
     for person in tmp:
         avg += person.rank;
@@ -116,44 +118,49 @@ def simple_population(f, method, para_dim, n=1000, basis_size=20):
     ranks = [0]*n;
 
     
-    for N in xrange(n):
-        parameters = random.random(size=[basis_size, para_dim])
+    for N in range(n):
+        parameters = random.random(size=[basis_size, para_dim])*2 - 1
         
         person = method(parameters, f);
         person.set_region(-2,2, reset=True)
 
         #perform mutation_tresh positive evolutions
-        for i in xrange(mutation_thresh):
+        for i in range(mutation_thresh):
             person.evolve_parameter(1);
 
         population[N] = person
         ranks[N] = person.rank
 
-
-    return evolved(population, ranks);
-    
+    tmp = evolved(population, ranks);
+    return tmp;
 
 
 def iterate(population):
     n = len(population)
 
     ranks = [0]*n
-    for N in xrange(n):
+    for N in range(n):
        
         #perform mutation_tresh positive evolutions
-        for i in xrange(mutation_thresh):
+        for i in range(mutation_thresh):
             population[N].evolve_parameter(1);
 
         ranks[N] = population[N].rank
 
-    return evolved(population, ranks)
+    tmp = evolved(population, ranks)
+
+    return tmp;
 
 
 
 
 
 
-
+def copy_list(list1):
+    list0 = []
+    for element in list1:
+        list0.append(element)
+    return list0
 
 
 
@@ -163,36 +170,55 @@ def iterate(population):
 
 
 ##GLOBALS
-mutation_thresh = 10
-max_num_kids = 5;
+mutation_thresh = 20
+max_num_kids = mutation_thresh/2;
 
 def main():
 
-    f = lambda x: exp(-x*x)
+    f = lambda x: x*x*exp(-x*x)
+    threshold = 1.2 #a decrease in functionality by (threshold - 1)*100% will not make your population die.
     
-    population = simple_population(f, sinebased, 2, n=500, basis_size=20)
-
+    population = simple_population(f, sinebased, 2, n=1000, basis_size=mutation_thresh)
+    best = population[0].rank;
+    all_time_best = best;
+    print "Best: ", best
+    
+    genetic_elite = copy_list(population)
+    
     for i in range(100):
         population = iterate(population)
 
-        print "Best: ", population[0].rank
-        
+        new_best = population[0].rank
+        print "Best: ", new_best
+
+        #Evolution made the wrong turn...
+        if new_best > best*threshold:
+            population = copy_list(genetic_elite)        
+
+        else:
+            if best < all_time_best:
+                all_time_best = best;
+                super_elite = copy_list(population)
+                
+            best = new_best
+            genetic_elite = copy_list(population)
+
+            x = population[0].x
+            fx = f(x);
+            figure(1)
             
-        x = population[0].x
-        fx = f(x);
-        figure(1)
-        
-        plot(x, fx, 'b')
-        axis([min(x)*1.2, max(x)*1.2, min(fx)*1.2, max(fx)*1.2])
-        hold("on")
-        for i in population[0].values:
-            print i
-        
-        plot(x, population[0].values, 'g')
-        hold("off")
+            plot(x, fx, 'b')
+            axis([min(x)*1.2, max(x)*1.2, min(fx)*1.2, max(fx)*1.2])
+            hold("on")
+       
+            plot(x, population[0].values, 'g')
+            hold("off")
 
 
-
+    ofile = open('out.dat', 'w')
+    for x, value in zip(super_elite[0].x, super_elite[0].values):
+        ofile.write(str(x) +"\t" + str(value) + "\n")
+    ofile.close()
     
 
 if __name__ == "__main__":
